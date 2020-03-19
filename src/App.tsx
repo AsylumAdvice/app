05d1f4301f763828ec58e59ee8ff0c0062f635b9
Steve Thorpe <thorpe.steve@gmail.com>
2020-03-19 16:07:20 +0100

    React app (#30)
    * initial react commit
    
    * cleanup
    
    * .gitignore yarn.lock
    
    * .gitignore package-lock.json ;)

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './asylum-advice-logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Goin back to my roots!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
