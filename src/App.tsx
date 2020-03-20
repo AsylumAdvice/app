191c8713ec52cd7b24d9a223db699d43f151be8e
Qaiser Abbasi <qabbasi@users.noreply.github.com>
2020-03-20 14:51:16 +0100

    gh pages (#33)
    add gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Hello world! lol
         </p>
         <p>
           Goin back to my roots!
