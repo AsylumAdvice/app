dbcf4b8e958f92fbca752f8461d4a65934b7eb0e
Qaiser Abbasi <qabbasi@users.noreply.github.com>
2020-03-20 14:53:04 +0100

    fix test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world! lol
+          Hello world!
         </p>
         <p>
           Goin back to my roots!
