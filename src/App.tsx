926ead78076025e9eacc36cb40e39a684445852b
Qaiser Abbasi <qabbasi@users.noreply.github.com>
2020-03-20 15:22:48 +0100

    material-ui (#32)
    Install material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import logo from './asylum-advice-logo.svg';
 import './App.css';
+import MetaTags from 'react-meta-tags';
 
-function App() {
-  return (
+export default function App() {
+  return <>
+    <MetaTags> /** https://material-ui.com/getting-started/usage/#responsive-meta-tag */
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+    </MetaTags>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
-        </p>
-        <p>
-          Goin back to my roots!
+          AsylumAdvice
         </p>
       </header>
     </div>
-  );
+  </>
 }
-
-export default App;
